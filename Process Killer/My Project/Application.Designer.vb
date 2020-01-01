449420431d0e4c199042385084404ccf9e71aa84
Bugzy <bugzy@noreply.com>
2020-01-01 23:54:33 +0000

    ignore build files

--- Process Killer/My Project/Application.Designer.vb
+++ Process Killer/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Process_Killer.Form1
+            Me.MainForm = Global.Process_Killer.Main
         End Sub
     End Class
 End Namespace
