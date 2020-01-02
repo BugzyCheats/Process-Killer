ddf83c54cea29dedde3ffaa412e7e8391557b625
Bugzy <bugzy@noreply.com>
2020-01-02 00:00:39 +0000

    ubuntu theme

---
+++ Process Killer/Forms/Main.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.UbuntuTheme1 = New Process_Killer.UbuntuTheme()
+        Me.SuspendLayout()
+        '
+        'UbuntuTheme1
+        '
+        Me.UbuntuTheme1.BackColor = System.Drawing.Color.FromArgb(CType(CType(25, Byte), Integer), CType(CType(25, Byte), Integer), CType(CType(25, Byte), Integer))
+        Me.UbuntuTheme1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.UbuntuTheme1.Location = New System.Drawing.Point(0, 0)
+        Me.UbuntuTheme1.Name = "UbuntuTheme1"
+        Me.UbuntuTheme1.Size = New System.Drawing.Size(800, 450)
+        Me.UbuntuTheme1.TabIndex = 0
+        Me.UbuntuTheme1.Text = "UbuntuTheme1"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.UbuntuTheme1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Main"
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents UbuntuTheme1 As UbuntuTheme
+End Class
